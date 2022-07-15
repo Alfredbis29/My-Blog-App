26bc33ad2b0e0256bf7bddf8426eedc8f7ace921
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-16 01:10:20 +0200

    Add the some validations for the Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: { message: 'Name field can not left blank' }
-  validates :posts_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   private
 
