66f9af2962381c64339d5eeae071e125245a7e01
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-15 22:18:25 +0200

    Give default value to the post_counter column and assign increment method to it

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
 end
