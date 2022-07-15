09017168f9e1ca38b29166e28dc0392e7da11120
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-16 01:00:23 +0200

    Add the following for the User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  
+
   after_save :update_posts_counter
 
   private
