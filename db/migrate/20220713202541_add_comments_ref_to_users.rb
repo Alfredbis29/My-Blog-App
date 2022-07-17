7330904a4b75fef4bf46634c5bb2d0391624bd6e
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-17 11:18:07 +0200

    fix

--- db/migrate/20220713202541_add_comments_ref_to_users.rb
+++ db/migrate/20220713202541_add_comments_ref_to_users.rb
@@ -1,5 +1,5 @@
 class AddCommentsRefToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_reference :users, :comments, null: false, foreign_key: true
+    add_reference :users, :comments, foreign_key: true
   end
 end
