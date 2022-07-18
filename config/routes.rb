5db8900bd03c54391132f8b588a8ffe22d64d6f2
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-18 19:00:48 +0200

    connect a router

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :posts
-  resources :comments
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "hello#index"
+  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
-  get "/hello" => "hello#index"
+  root to: 'users#index'
 end
