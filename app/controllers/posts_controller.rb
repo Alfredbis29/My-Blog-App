18ebf130ffea3df4328b932f91a321c6294524c9
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-19 21:41:42 +0200

    add view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: { message: 'Name field can not left blank' }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  private
+  # private
 
   def most_recent_posts
     posts.order('created_at DESC').limit(3)
