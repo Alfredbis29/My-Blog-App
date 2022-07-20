dba84769a4ac9841e9b545ca8befbba7c28e9a54
Alfredbis29 <kalumunabisimwa5@gmail.com>
2022-07-20 22:01:49 +0200

    testing

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,14 +1,13 @@
+require 'rails_helper'
+
 RSpec.describe 'Users Controller', type: :request do
   describe 'GET /index' do
     context 'when the page is loaded' do
-      before do
-        get '/users'
-      end
+      before(:example) {get '/users'}
       it 'returns a 200 status code' do
         expect(response).to have_http_status(200)
       end
       it 'renders the index template' do
-        expect(response).to render_template('index')
         expect(response.body).to render_template('index')
       end
       it 'includes number of posts' do
@@ -16,22 +15,4 @@ RSpec.describe 'Users Controller', type: :request do
       end
     end
   end
-
-  describe 'GET /show' do
-    context 'when the page is loaded' do
-      before do
-        get user_path(user.id)
-      end
-      it 'returns a 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-      it 'renders the show template' do
-        expect(response).to render_template('show')
-        expect(response.body).to render_template('show')
-      end
-      it 'includes see all posts' do
-        expect(response.body).to include('See all posts')
-      end
-    end
-  end
- end
+end
