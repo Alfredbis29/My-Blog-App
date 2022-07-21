68a67f12bb80d621514423c035ab8788fbeddda3
Alfredbis29 <Kalumunabisimwa@gmail.com>
2022-07-22 00:21:32 +0200

    modify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,12 @@
-class Post < ApplicationRecord
-  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+class User < ApplicationRecord
+  validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
 
-  after_save :update_posts_counter
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  private
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
