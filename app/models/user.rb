5986186efc340b3a3087690540d066deb7f9dff5
Alfredbis29 <Kalumunabisimwa@gmail.com>
2022-07-21 17:33:26 +0200

    user to add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
-class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+class Post < ApplicationRecord
+  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  validates :name, presence: { message: 'Name field can not left blank' }
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 
-  # private
+  after_save :update_posts_counter
 
-  def most_recent_posts
-    posts.order('created_at DESC').limit(3)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
