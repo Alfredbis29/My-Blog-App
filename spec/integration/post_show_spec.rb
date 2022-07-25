91de6ff7cc4cd46244caf4a129f2d4c4e7285036
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-07-25 16:42:04 +0200

    test show and index for post

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Post show', type: :feature do
   describe 'Post' do
     before(:each) do
-      @first_user = User.create(name: 'Tom', photo: 'margaret.jpeg', bio: 'Teacher from Mexico.')
-      @second_user = User.create(name: 'Lilly', photo: 'margaret.jpeg', bio: 'Teacher from Poland.')
-
+      @first_user = User.create(name: 'Tom', photo: 'margaret.jpeg', bio: 'Teacher from Mexico.', posts_counter: 0)
+      @first_user.save!
+      @second_user = User.create(name: 'Lilly', photo: 'margaret.jpeg', bio: 'Teacher from Poland.', posts_counter: 0)
+      @second_user.save!
       visit root_path
 
       @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post',
-                                comments_counter: 0, like_counter: 0)
+                                comments_counter: 0, likes_counter: 0)
       @second_post = Post.create(author: @first_user, title: 'Hello, again', text: 'This is my second post',
-                                 comments_counter: 0, like_counter: 0)
+                                 comments_counter: 0, likes_counter: 0)
       @third_post = Post.create(author: @second_user, title: 'Hello', text: 'This is my first post',
-                                comments_counter: 0, like_counter: 0)
+                                comments_counter: 0, likes_counter: 0)
       @fourth_post = Post.create(author: @second_user, title: 'Hello, again', text: 'This is my second post',
-                                 comments_counter: 0, like_counter: 0)
+                                 comments_counter: 0, likes_counter: 0)
 
       @comment1 = Comment.create(post: Post.first, author: User.first, text: 'Hi Tom!')
       @comment2 = Comment.create(post: Post.first, author: User.first, text: 'Hi Tom Again!')
@@ -29,7 +30,7 @@ RSpec.describe 'Post show', type: :feature do
     end
 
     it 'shows the person who wrote the post' do
-      expect(page).to have_content('Margaret')
+      expect(page).to have_content('Tom')
     end
 
     it 'shows number of comments' do
@@ -39,7 +40,7 @@ RSpec.describe 'Post show', type: :feature do
 
     it 'shows number of likes' do
       post = Post.first
-      expect(page).to have_content(post.like_counter)
+      expect(page).to have_content(post.likes_counter)
     end
 
     it 'can see the post\'s body.' do
--- spec/integration/posts_index_spec.rb
+++ spec/integration/posts_index_spec.rb
@@ -1,40 +1,77 @@
-class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
+require 'rails_helper'
 
-  def index
-    @user = current_user
-    @posts = @user.posts
-  end
+RSpec.describe 'posts#index', type: :feature do
+  describe 'Post' do
+    before(:each) do
+      @user1 = User.create(name: 'Margaret', photo: 'margaret.jpeg', bio: 'bio', posts_counter: 0)
+      @user1.save!
+      visit root_path
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-    @user = current_user
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.author_id = current_user.id
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to user_post_url(current_user, @post), notice: 'Post was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+      @post1 = Post.create(title: 'First Post', text: 'This is my first post', comments_counter: 0, likes_counter: 0,
+                           author: @user1)
+      @post2 = Post.create(title: 'Second Post', text: 'This is my second post', comments_counter: 0, likes_counter: 0,
+                           author: @user1)
+      @post3 = Post.create(title: 'Third Post', text: 'This is my third post', comments_counter: 0, likes_counter: 0,
+                           author: @user1)
+      @comment1 = Comment.create(text: 'Comment 1!', author: User.first,
+                                 post: Post.first)
+      @comment2 = Comment.create(text: 'Comment 2!', author: User.first, post: Post.first)
+      @comment3 = Comment.create(text: 'Comment 2!', author: User.first, post: Post.first)
     end
-  end
 
-  private
+    it "shows user's profile picture" do
+      visit(user_posts_path(@user1.id))
+      expect(page).to have_css('img[src*="margaret.jpeg"]')
+    end
 
-  def set_post
-    @post = Post.find(params[:id])
-  end
+    it 'shows the users username' do
+      visit(user_posts_path(@user1.id))
+      expect(page).to have_content('Margaret')
+    end
 
-  def post_params
-    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+    it 'shows number of posts of user has written' do
+      visit(user_posts_path(@user1.id))
+      post = Post.all
+      expect(post.size).to eql(3)
+    end
+
+    it 'shows number of posts by user' do
+      visit(user_posts_path(@user1.id))
+      user = User.first
+      expect(page).to have_content(user.posts_counter)
+    end
+
+    it 'shows posts title' do
+      visit(user_posts_path(@user1.id))
+      expect(page).to have_content('First Post')
+    end
+
+    it 'can see some of the post detail' do
+      visit(user_posts_path(@user1.id))
+      expect(page).to have_content 'This is my first post'
+    end
+
+    it 'can see the first comment on a post' do
+      visit(user_posts_path(@user1.id))
+      expect(page).to have_content 'Comment 1'
+    end
+
+    it 'can see how many comments a post has.' do
+      visit(user_posts_path(@user1.id))
+      post = Post.first
+      expect(page).to have_content(post.comments_counter)
+    end
+
+    it 'can see how many likes a post has.' do
+      visit(user_posts_path(@user1.id))
+      post = Post.first
+      expect(page).to have_content(post.likes_counter)
+    end
+
+    it "redirects the user to the post's show page after clickin on it" do
+      visit(user_posts_path(@user1.id))
+      click_link 'First Post'
+      expect(page).to have_current_path user_post_path(@post1.author_id, @post1)
+    end
   end
 end
