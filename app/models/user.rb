913044293a323aa2847c9535b46a228f39dc8d38
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-07-27 16:31:06 +0200

    add devise in my user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,25 @@
 class User < ApplicationRecord
-  validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, allow_blank: false
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
+
+  after_initialize :set_defaults
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_defaults
+    self.posts_counter ||= 0
   end
 end
