f8f6590a795ef33343a5ad5fdd0ff82e81c06330
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-07-27 17:16:24 +0200

    migrate devise

--- db/migrate/20220715190449_create_comment.rb
+++ db/migrate/20220715190449_create_comment.rb
@@ -1,9 +1,9 @@
-class CreateComment < ActiveRecord::Migration[7.0]
+class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :author, null: false, foreign_key: { to_table: 'users'}
-      t.references :post, null: false, foreign_key: true
+      t.references :author, index: true, foreign_key: { to_table: :users }
+      t.references :post, index: true, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
